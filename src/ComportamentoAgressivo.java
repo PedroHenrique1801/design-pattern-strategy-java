a22960cdf23c1748f20eff970ee174d804ea5c42
Pedro Henrique <pedrohsantos1801@gmail.com>
2026-08-05 15:10:10 -0300

    Fix formatting in ComportamentoAgressivo class

--- src/ComportamentoAgressivo.java
+++ src/ComportamentoAgressivo.java
@@ -1,6 +1,6 @@
 public class ComportamentoAgressivo implements Comportamento {
     @Override
     public void mover() {
-        System.out.println("Robô movendo-se de forma AGRESSIVA (preparado para ataque)...");
+        System.out.println("Robô movendo-se de forma AGRESSIVA (preparado para ataque)...");  
     }
 }
