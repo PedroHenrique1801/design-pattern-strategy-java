f6c1d24c601f720452227537780f1d829f3dbcc8
Pedro Henrique <pedrohsantos1801@gmail.com>
2026-08-28 09:16:44 -0300

    Update ComportamentoDefensivo.java

--- src/ComportamentoDefensivo.java
+++ src/ComportamentoDefensivo.java
@@ -1,6 +1,6 @@
 public class ComportamentoDefensivo implements Comportamento {
     @Override
     public void mover() {
-       
+        System.out.println("Robô movendo-se de forma defensiva (cautelosamente)...");
     }
 }
